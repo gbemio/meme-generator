409e26b8fbb328d6efd956a20c0d9bcfd487641e
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-24 15:31:42 +0100

    Added and styled Meme Component

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Meme = () => {
-  return <div></div>;
+  return (
+    <div>
+      <form action="" className="form--section">
+        <div>
+          <input type="text" placeholder="shut up" className="input1" />
+          <input
+            type="text"
+            placeholder="and take my money"
+            className="input2"
+          />
+        </div>
+        <button className="meme--btn">Get a new meme image</button>
+      </form>
+    </div>
+  );
 };
 
 export default Meme;
