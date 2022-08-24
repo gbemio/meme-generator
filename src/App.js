ffde0c7ebe499cd92cce56789d69a9de89af05aa
Gbemisola Odukoya <folaryz@gmail.com>
2022-08-24 14:25:58 +0100

    Created Meme Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header.jsx";
+import Meme from "./Meme.jsx";
 
 const App = () => {
   return (
     <div className="container">
       <Header />
+      <Meme />
     </div>
   );
 };
